396ce2afd240cbe2c18351b8c4370a646bca7e74
Winter Evans <41024059+TheWinterSkye@users.noreply.github.com>
2023-03-20 16:22:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const CLIENT_ID = '466955074065-bbu0i5sb3aqrnjm6e6k3jd2lq39hpahs.apps.googleusercontent.com';
 const CLIENT_SECRET = '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';
 const SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
-const REDIRECT_URI = 'https://spectacular-daifuku-b382f4.netlify.app';
+const REDIRECT_URI = 'https://spectacular-daifuku-b382f4.netlify.app/';
 
 const form = document.getElementById('form');
 const submitBtn = document.getElementById('submit_btn');
@@ -30,7 +30,7 @@ gapi.load('client:auth2', () => {
 // Function to send data to Google Sheet
 async function sendDataToGoogleSheet(data) {
   const spreadsheetId = '1MeEk6IUBWZwP8fNwcOKkF6ZhIyKscV_l6gVd8Derih0';
-  const sheetName = 'Sheet1';
+  const sheetName = 'EMS TRANSPORT CHARTS';
   const range = `${sheetName}!A:F`;
 
   await gapi.client.sheets.spreadsheets.values.append({
