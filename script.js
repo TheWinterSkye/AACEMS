c196ab79847ef6dba25efdc0a6687250fd7ba719
Winter Evans <41024059+TheWinterSkye@users.noreply.github.com>
2023-03-20 16:30:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 // Replace with your own client ID and client secret
-const CLIENT_ID = '466955074065-bbu0i5sb3aqrnjm6e6k3jd2lq39hpahs.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
+const CLIENT_ID = 'your-client-id';
+const CLIENT_SECRET = 'your-client-secret';
 const SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
-const REDIRECT_URI = 'https://spectacular-daifuku-b382f4.netlify.app/';
+const REDIRECT_URI = 'your-redirect-uri';
 
 const form = document.getElementById('form');
 const submitBtn = document.getElementById('submit_btn');
 
-// Google API OAuth2 setup
-gapi.load('client:auth2', () => {
-  gapi.client.init({
-    clientId: CLIENT_ID,
-    clientSecret: CLIENT_SECRET,
-    scope: SCOPE,
-    redirectUri: REDIRECT_URI,
-  }).then(() => {
-    // Check if user is already signed in
-    if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
-      submitBtn.disabled = false;
-    } else {
-      // If not signed in, prompt user to sign in
-      gapi.auth2.getAuthInstance().signIn().then(() => {
-        submitBtn.disabled = false;
-      });
-    }
+let accessToken;
+
+// Function to get access token using client ID and client secret
+async function getAccessToken() {
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPE}&access_type=offline`;
+
+  const authCode = await new Promise((resolve) => {
+    const authWindow = window.open(authUrl, '_blank', 'width=800,height=600');
+    window.addEventListener('message', (event) => {
+      if (event.origin === window.location.origin) {
+        resolve(event.data);
+        authWindow.close();
+      }
+    });
   });
-});
+
+  const tokenResponse = await fetch(`https://www.googleapis.com/oauth2/v4/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      code: authCode,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      redirect_uri: REDIRECT_URI,
+      grant_type: 'authorization_code',
+    }),
+  });
+
+  const tokenData = await tokenResponse.json();
+  return tokenData.access_token;
+}
 
 // Function to send data to Google Sheet
 async function sendDataToGoogleSheet(data) {
@@ -33,20 +47,28 @@ async function sendDataToGoogleSheet(data) {
   const sheetName = 'EMS TRANSPORT CHARTS';
   const range = `${sheetName}!A:F`;
 
-  await gapi.client.sheets.spreadsheets.values.append({
-    spreadsheetId: spreadsheetId,
-    range: range,
-    valueInputOption: 'RAW',
-    insertDataOption: 'INSERT_ROWS',
-    resource: {
-      values: [data]
+  await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}:append`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`,
     },
+    body: JSON.stringify({
+      range,
+      majorDimension: 'ROWS',
+      values: [data],
+      valueInputOption: 'RAW',
+      insertDataOption: 'INSERT_ROWS',
+    }),
   });
 }
 
-// Event listener for form submission
-form.addEventListener('submit', (e) => {
+submitBtn.addEventListener('click', async (e) => {
   e.preventDefault();
+
+  if (!accessToken) {
+    accessToken = await getAccessToken();
+  }
 
   // Get form data
   const patients_name = form.patients_name.value;
@@ -65,7 +87,7 @@ form.addEventListener('submit', (e) => {
     alert('Data submitted successfully!');
     form.reset();
   }).catch((error) => {
-    console.error('Error sending data to Google Sheet:', error);
-    alert('Failed to submit data. Please try again.');
-  });
+    console.error('Error sending data toGoogle Sheet:', error);
+alert('Failed to submit data. Please try again.');
+});
 });
