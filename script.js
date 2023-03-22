d79b983368f92241ef57380cc52f6a81c8de337c
Winter Evans <41024059+TheWinterSkye@users.noreply.github.com>
2023-03-22 11:04:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,45 @@
-const API_KEY = 'AIzaSyAUoGZe5leyZmnH5vAsmInhD1HmDm_uxI4';
+async function submitForm(event) {
+  event.preventDefault();
 
-const form = document.getElementById('form');
-const submitBtn = document.getElementById('submit_btn');
+  const apiUrl = 'YOUR_SHEETDB_API_URL';
 
-// Function to send data to Google Sheet
-async function sendDataToGoogleSheet(data) {
-  const spreadsheetId = '1jtLnv4rQrIowGNmfulONS0HSY1Il2eu-pv0DIko6H3g';
-  const sheetName = 'sheet';
-  const range = 'sheet!A:F';
+  const form = document.getElementById('form');
+  const formData = new FormData(form);
+  const medication = [];
 
-  await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}:append?key=${API_KEY}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      range,
-      majorDimension: 'ROWS',
-      values: [data],
-      valueInputOption: 'RAW',
-      insertDataOption: 'INSERT_ROWS',
-    }),
+  formData.getAll('medication[]').forEach((value) => {
+    medication.push(value);
   });
+
+  const data = {
+    "data": [
+      {
+        "patients_name": formData.get('patients_name'),
+        "cid": formData.get('cid'),
+        "injury_code": formData.get('injury_code'),
+        "medication": medication.join(', '),
+        "notes": formData.get('notes')
+      }
+    ]
+  };
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+
+    if (response.ok) {
+      alert('Form submitted successfully');
+      form.reset();
+    } else {
+      alert('Error submitting form');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    alert('Error submitting form');
+  }
 }
-
-submitBtn.addEventListener('click', async (e) => {
-  e.preventDefault();
-
-  // Get form data
-  const patients_name = form.patients_name.value;
-  const cid = form.cid.value;
-  const injury_code = form.injury_code.value;
-  const medications = Array.from(form['medication[]']).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
-  const notes = form.notes.value;
-
-  const chartNumber = new Date().toISOString().replace(/[-:.]/g, '');
-
-  // Prepare data for Google Sheet
-  const data = [chartNumber, patients_name, cid, injury_code, medications.join(', '), notes];
-
-  // Send data to Google Sheet
-  sendDataToGoogleSheet(data).then(() => {
-    alert('Data submitted successfully!');
-    form.reset();
-  }).catch((error) => {
-    console.error('Error sending data to Google Sheet:', error);
-    alert('Failed to submit data. Please try again.');
-  });
-});
