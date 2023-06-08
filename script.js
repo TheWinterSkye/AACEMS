04866a3f8a5ab4d28e14bb7fcdcf9bafd7f35acd
Winter Evans <41024059+TheWinterSkye@users.noreply.github.com>
2023-06-08 18:52:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,5 @@
-async function submitForm(event) {
-  event.preventDefault();
-
-  const apiUrl = 'https://sheetdb.io/api/v1/amxeepufjfzvc';
-
-  const form = document.getElementById('form');
-  const formData = new FormData(form);
-  const medication = [];
-
-  formData.getAll('medication[]').forEach((value) => {
-    medication.push(value);
-  });
-
-  const data = {
-    "data": [
-      {
-        "patients_name": formData.get('patients_name'),
-        "cid": formData.get('cid'),
-        "injury_code": formData.get('injury_code'),
-        "medication": medication.join(', '),
-        "notes": formData.get('notes')
-      }
-    ]
-  };
-
-  try {
-    const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-
-    if (response.ok) {
-      alert('Form submitted successfully');
-      form.reset();
-    } else {
-      alert('Error submitting form');
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    alert('Error submitting form');
-  }
-}
+document.getElementById('pic-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Stop the form from submitting normally
+    var url = document.getElementById('pic-url').value;
+    document.getElementById('character-pic').src = url;
+});
