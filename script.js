88df4894988168a9b6f57d7dc8c1cfcf47a54941
Winter Evans <41024059+TheWinterSkye@users.noreply.github.com>
2023-06-08 19:21:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-// script.js
-function updateImage() {
-    var url = document.getElementById('imageURL').value;
-    var img = document.getElementById('characterImage');
+let skills = [];
+let stunts = [];
 
-    // Check if the URL is valid
-    if(url.trim() !== '') {
-        img.src = url;
+function printInput(inputId, outputId) {
+    let input = document.getElementById(inputId).value;
+    document.getElementById(outputId).innerText = input;
+}
+
+function updateImage() {
+    let newURL = document.getElementById('imageURL').value;
+    document.getElementById('characterImage').src = newURL;
+}
+
+function addSkill() {
+    if (skills.length < 20) {
+        skills.push(document.getElementById('skillsInput').value);
+        document.getElementById('skillsOutput').innerText = skills.join(", ");
+    }
+}
+
+function addStunt() {
+    if (stunts.length < 20) {
+        stunts.push(document.getElementById('stuntsInput').value);
+        document.getElementById('stuntsOutput').innerText = stunts.join(", ");
     }
 }
